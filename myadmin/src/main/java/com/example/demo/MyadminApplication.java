c6b7d8a106cd3709317221a215c402b13197c3ee
yang <pc@DESKTOP-38I18KD>
2021-08-06 13:56:21 +0800

    增加监控FEBS

--- myadmin/src/main/java/com/example/demo/MyadminApplication.java
+++ myadmin/src/main/java/com/example/demo/MyadminApplication.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 /**
  * 
@@ -10,12 +16,39 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
  *应用对象
  */
 
-@EnableAdminServer
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
+@Configuration
 public class MyadminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyadminApplication.class, args);
 	}
+	
+	@Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
+        }
+    }
 
 }
